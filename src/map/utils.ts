20a00e6537a77400e3c1075dcceca6c8969d7abe
zwarn <Michael.Budahn@gmail.com>
2020-02-09 23:04:51 +0100

    Switch to a saner hexagonal model

--- src/map/utils.ts
+++ src/map/utils.ts
@@ -1,31 +1,22 @@
 const terrainWidth = 64;
-const terrainHeight = 80;
+const terrainHeight = 64;
 
 export const yIndex2YPosi = (y: number) =>
-  y * terrainHeight + (y === 0 ? 0 : -15 * y);
+    y * terrainHeight;
 export const xIndex2XPosi = (x: number, y: number) =>
-  x * terrainWidth + (y % 2 === 0 ? terrainWidth / 2 : 0);
- 
+    x * terrainWidth + y * terrainHeight / 2;
+
 export const isAdjacent = (
-  x1: number, y1: number, x2: number, y2: number
+    x1: number, y1: number, x2: number, y2: number
 ) => {
-  const neighbors =  y1 % 2 === 1 
-    ? [
-      [x1 - 1, y1 - 1],
-      [x1, y1 - 1],
-      [x1 + 1, y1],
-      [x1, y1 + 1],
-      [x1 - 1 , y1 + 1],
-      [x1 - 1, y1],
-    ]
-    : [
-      [x1, y1 - 1],
-      [x1 + 1, y1 - 1],
-      [x1 + 1, y1],
-      [x1 + 1, y1 + 1],
-      [x1, y1 + 1],
-      [x1 - 1, y1],
-    ];
-  const neighbor = neighbors.find(([x, y]) => x === x2 && y === y2);
-  return Boolean(neighbor);
+    const neighbors = [
+            [x1+1, y1],
+            [x1-1, y1],
+            [x1, y1+1],
+            [x1, y1-1],
+            [x1+1, y1-1],
+            [x1-1, y1+1],
+        ];
+    const neighbor = neighbors.find(([x, y]) => x === x2 && y === y2);
+    return Boolean(neighbor);
 };
