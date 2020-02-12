65aec02b1a3f820c0e4622c26e818f1a2237b31f
zwarn <michael.budahn@gmail.com>
2020-02-12 18:09:09 +0100

    add monsters

--- src/view/ui.ts
+++ src/view/ui.ts
@@ -2,12 +2,14 @@ import * as PIXI from 'pixi.js';
 import { GameState } from "../model/gameState";
 import { Tile } from "../model/tile";
 import { Point } from "../model/position";
+import { Monster } from '../model/monster';
 
 const tileSize: number = 64;
 
 export class Ui {
     boardContainer: PIXI.Container
     tiles: PIXI.Sprite[][] = [];
+    monsters: PIXI.Sprite[] = [];
 
 
     static createUi(state: GameState): Ui {
@@ -19,18 +21,37 @@ export class Ui {
 
     private createUi(state: GameState) {
         this.boardContainer = new PIXI.Container();
+        this.createBoard(state);
+        this.createMonsters(state);
+
+        this.boardContainer.position.set(tileSize, tileSize);
+    }
+
+    private createMonsters(state: GameState) {
+        state.monsters.forEach(monster => this.createMonster(monster));
+    }
+
+    private createMonster(monster: Monster) {
+        let x = monster.position.x;
+        let y = monster.position.y;
+        let sprite = PIXI.Sprite.from("Assets/Images/Monster/" + monster.name + ".png")
+        let dc = this.toDisplayCoords(x,y);
+        sprite.anchor.set(0.5,0.5);
+        sprite.position.set(dc.x,dc.y);
+        this.boardContainer.addChild(sprite);
+        this.monsters.push(sprite);
+    }
+
+    private createBoard(state: GameState) {
         let size = state.map.tiles.length;
         this.tiles = [];
         for (let x = 0; x < size; x++) {
-            const row = []
+            const row = [];
             for (let y = 0; y < size; y++) {
-                row.push(this.createTile(state.map.tiles[x][y]))
+                row.push(this.createTile(state.map.tiles[x][y]));
             }
-            this.tiles.push(row)
+            this.tiles.push(row);
         }
-        
-        this.boardContainer.position.set(tileSize, tileSize);
-
     }
 
     private createTile(tile: Tile): PIXI.Sprite {
@@ -48,4 +69,5 @@ export class Ui {
     toDisplayCoords(x: number, y: number): Point {
         return new Point(x * tileSize + y * tileSize / 2, y * tileSize);
     }
+
 }
