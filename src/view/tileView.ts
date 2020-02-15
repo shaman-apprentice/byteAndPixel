97ab03b14497feeddf042fcf34157a001869239e
zwarn <michael.budahn@gmail.com>
2020-02-15 13:29:20 +0100

    turn terrain images and fix hitboxes

--- src/view/tileView.ts
+++ src/view/tileView.ts
@@ -15,11 +15,10 @@ export class TileView {
         let y = tile.position.y;
         let dc = Ui.toDisplayCoords(x, y);
         this.sprite.anchor.set(0.5, 0.5);
-        this.sprite.angle = 90;
         this.sprite.position.set(dc.x, dc.y);
         this.sprite.interactive = true;
         //hitarea und anchor arbeiten nicht gut zusammen
-        this.sprite.hitArea = new PIXI.Polygon([-32+0, -32+15, -32+0, 64, -32+30, -32+79, -32+33, -32+79, -32+63, -32+64, -32+63, -32+15, -32+33, -32+0, -32+30, -32+0]);
+        this.sprite.hitArea = new PIXI.Polygon([-32+0, -40+15, -32+0, -40+64, -32+30, -40+79, -32+33, -40+79, -32+63, -40+64, -32+63, -40+15, -32+33, -40+0, -32+30, -40+0]);
         this.sprite.on('click', (() => this.onClick(x, y)).bind(this));
         container.addChild(this.sprite);
     }
