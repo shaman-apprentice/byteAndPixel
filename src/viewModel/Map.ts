196465532b71dbb13234574571359459304c2944
zwarn <Michael.Budahn@gmail.com>
2020-02-23 22:38:05 +0100

    adjust to smaller tilesize

--- src/viewModel/Map.ts
+++ src/viewModel/Map.ts
@@ -21,7 +21,7 @@ export class Map implements IGuiElem {
   private createTileSprite(tileData: Tile) {
     const sprite = PIXI.Sprite.from('Assets/Images/Terrain/' + tileData.terrainType.toString() + '.png')
     sprite.anchor.set(0.5, 0.5);
-    sprite.hitArea = new PIXI.Polygon([-32+0, -40+15, -32+0, -40+64, -32+30, -40+79, -32+33, -40+79, -32+63, -40+64, -32+63, -40+15, -32+33, -40+0, -32+30, -40+0]);
+    sprite.hitArea = new PIXI.Polygon([-32+0, -32+15, -32+0, -32+64, -32+30, -32+79, -32+33, -32+79, -32+63, -32+64, -32+63, -32+15, -32+33, -32+0, -32+30, -32+0]);
     const dc = tileData.position.toDisplayCoords();
     sprite.position.set(dc.x, dc.y);
     sprite.interactive = true;
