85164e1582e2daeea5f34184e90aa2a52f3ac8b2
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-05-01 16:09:59 +0200

    PoC

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Application } from 'pixi.js';
 import { Ui } from './viewModel/ui';
+import { Position } from './viewModel/Position';
+
+// https://github.com/shaman-apprentice/pack-mule
+import { PMap } from '@shaman-apprentice/pack-mule'
 
 const app = new Application({
   backgroundColor: 0x1099bb
@@ -11,3 +15,8 @@ const ui = new Ui();
 
 app.stage.addChild(ui.boardContainer);
 app.stage.addChild(ui.statusContainer);
+
+const m = new PMap<Position, number>();
+const p = new Position(1, 2);
+m.add(p, 9001);
+console.log(m.get(p));
