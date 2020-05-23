9b22539739ba9748f43035e499754a81811e34c3
zwarn <michael.budahn@gmail.com>
2020-05-23 17:19:38 +0200

    Add spider enemy image

--- src/viewModel/utils/monster.ts
+++ src/viewModel/utils/monster.ts
@@ -7,7 +7,7 @@ import { ElementSignature } from "./Element";
 export const getInitialMonsters = (): HashMap<number, Monster> => {
   const appleman = new Monster('appleman', new Position(2, 2), new ElementSignature(0,0,0,1,0));
   const flammie = new Monster('flammie', new Position(4, 1), new ElementSignature(0,1,0,0,0));
-  const enemy = new Monster('Dummymon', new Position(6, 6), new ElementSignature(0,0,0,0,0), false);
+  const enemy = new Monster('spider', new Position(6, 6), new ElementSignature(0,0,0,0,0), false);
   const monsters = [appleman, flammie, enemy];
   const map = new HashMap<number, Monster>(k => String(k));
   monsters.forEach(monster => map.set(monster.id, monster));
