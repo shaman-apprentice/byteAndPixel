a9ae9e76217be74e1adc0fa70ba6ffc45e4fa724
zwarn <michael.budahn@gmail.com>
2020-06-01 12:43:02 +0200

    #13 fix npe when deselecting no monster

--- src/controller/actions/SelectedMonsterChangeAction.ts
+++ src/controller/actions/SelectedMonsterChangeAction.ts
@@ -9,7 +9,9 @@ export class SelectedMonsterChangeAction extends Action {
 
   doAction() {
     const unselected = GameState.monsters.get(GameState.selectedMonster);
-    unselected.pixiElem.filters = removeFilter(unselected.pixiElem.filters, unselected.friendly, true);
+    if (unselected) {
+      unselected.pixiElem.filters = removeFilter(unselected.pixiElem.filters, unselected.friendly, true);
+    }
     GameState.selectedMonster = this.monster.id;
   }
 
