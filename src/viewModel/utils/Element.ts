af61414c25a2b2982ba614f4c69424e873ebd958
zwarn <michael.budahn@gmail.com>
2020-06-17 17:44:45 +0200

    give Elementsignature a better useable builder

--- src/viewModel/utils/Element.ts
+++ src/viewModel/utils/Element.ts
@@ -33,4 +33,14 @@ export class ElementSignature {
         return this.earth == other.earth && this.fire == other.fire && this.ice == other.ice && this.nature == other.nature && this.metal == other.metal;
     }
 
+    static build(element: Element, value: number = 1) {
+        switch(element) {
+            case Element.Earth: return new ElementSignature(value,0,0,0,0);
+            case Element.Fire: return new ElementSignature(0,value,0,0,0);
+            case Element.Ice: return new ElementSignature(0,0,value,0,0);
+            case Element.Nature: return new ElementSignature(0,0,0,value,0);
+            case Element.Metal: return new ElementSignature(0,0,0,0,value);
+        }
+    }
+
 }
--- src/viewModel/utils/monster.ts
+++ src/viewModel/utils/monster.ts
@@ -2,7 +2,7 @@ import { Monster, MonsterStats } from "../Monster";
 import { Position } from "../Position";
 import { GameState } from "../../GameState";
 import { HashMap } from "utils/HashMap";
-import { ElementSignature } from "./Element";
+import { Element, ElementSignature } from "./Element";
 import { Spider } from "viewModel/enemy/spider";
 import { Cave } from "viewModel/enemy/cave";
 
@@ -16,11 +16,11 @@ export const getInitialMonsters = (): HashMap<number, Monster> => {
   const flammie = new Monster('flammie', new Position(4, 1), flammieStats);
   const penguin = new Monster('penguin', new Position(1, 1), penguinStats);
   const watchhog = new Monster('watchhog', new Position(1, 4), watchhogStats);
-  const danjii = new Monster('danjii', new Position(2,3), danjiiStats);
-  const leefa = new Monster('leefa', new Position(0,0),leefaStats);
-  const coffy = new Monster('coffy',new Position(0,3),coffyStats);
+  const danjii = new Monster('danjii', new Position(2, 3), danjiiStats);
+  const leefa = new Monster('leefa', new Position(0, 0), leefaStats);
+  const coffy = new Monster('coffy', new Position(0, 3), coffyStats);
   const spider = new Spider('spider', new Position(6, 5), spiderStats);
-  const kyromon = new Monster('kyromon', new Position(0,1), kyromonStats);
+  const kyromon = new Monster('kyromon', new Position(0, 1), kyromonStats);
   const cave = new Cave('cave', new Position(6, 6), caveStats);
   const cave2 = new Cave('cave', new Position(11, 6), caveStats);
   const cave3 = new Cave('cave', new Position(6, 11), caveStats);
@@ -47,14 +47,14 @@ export const monsterIdAtPosition = (position: Position): number => {
   return monsterAtPosition(position)?.id ?? -1;
 }
 
+const danjiiStats: MonsterStats = new MonsterStats(ElementSignature.build(Element.Nature, 2), 8, 2);
+const leefaStats: MonsterStats = new MonsterStats(ElementSignature.build(Element.Nature, 3), 1, 3);
+const coffyStats: MonsterStats = new MonsterStats(ElementSignature.build(Element.Metal), 8, 2);
+const kyromonStats: MonsterStats = new MonsterStats(ElementSignature.build(Element.Ice, 2), 3, 3);
+const applemanStats: MonsterStats = new MonsterStats(ElementSignature.build(Element.Nature), 8, 6);
+const flammieStats: MonsterStats = new MonsterStats(ElementSignature.build(Element.Fire), 6, 5);
+const penguinStats: MonsterStats = new MonsterStats(ElementSignature.build(Element.Ice), 7, 4);
+const watchhogStats: MonsterStats = new MonsterStats(ElementSignature.build(Element.Earth), 10, 2);
 
-const danjiiStats: MonsterStats = new MonsterStats(new ElementSignature(0,0,0,2,0), 8, 2);
-const leefaStats: MonsterStats = new MonsterStats(new ElementSignature(0,0,0,3,0),1,3);
-const coffyStats: MonsterStats = new MonsterStats(new ElementSignature(0,0,0,0,1), 8,2);
-const kyromonStats: MonsterStats = new MonsterStats(new ElementSignature(0,0,2,0,0), 3, 3);
-const applemanStats: MonsterStats = new MonsterStats(new ElementSignature(0,0,0,1,0), 8, 6);
-const flammieStats: MonsterStats = new MonsterStats(new ElementSignature(0,1,0,0,0), 6, 5);
-const penguinStats: MonsterStats = new MonsterStats(new ElementSignature(0,0,1,0,0), 7, 4);
-const watchhogStats: MonsterStats = new MonsterStats(new ElementSignature(1,0,0,0,0), 10, 2);
-export const spiderStats: MonsterStats = new MonsterStats(new ElementSignature(0,0,0,0,0), 3, 1);
-const caveStats: MonsterStats = new MonsterStats(new ElementSignature(0,0,0,0,0), 10, 1);
+export const spiderStats: MonsterStats = new MonsterStats(new ElementSignature(0, 0, 0, 0, 0), 3, 1);
+const caveStats: MonsterStats = new MonsterStats(new ElementSignature(0, 0, 0, 0, 0), 10, 1);
