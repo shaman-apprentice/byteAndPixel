8ae3adc54ed5bfca8a274d87d261a0d6b3649842
Silbertiger <biebler@live.de>
2020-07-18 17:56:44 +0200

    a bubble instead of a whole background, but still does not register mouseover

--- src/viewModel/ui/ActionUI.ts
+++ src/viewModel/ui/ActionUI.ts
@@ -3,6 +3,9 @@ import {Monster} from "../Monster";
 import { GameState } from "GameState";
 import {StateChangeEvent} from "../../controller/events/StateChangeEvent";
 import { AttackAction } from 'controller/actions/AttackAction';
+import { MouseHoverEvent } from 'controller/events/MouseHoverEvent';
+import { Position } from 'viewModel/Position';
+import { GlowFilter } from "@pixi/filter-glow";
 
 export class ActionUI{
     container: PIXI.Container;
@@ -24,8 +27,8 @@ export class ActionUI{
             if(this.currentMonster.friendly){
                 const size = this.currentMonster.skillList.length;
                 for(let i = 0; i < size; i++){
-                    const action: ActionUiElement = new ActionUiElement("Assets/Images/Skills/"+this.currentMonster.skillList[i].name+".png" , this.currentMonster.skillList[i].name);
-                    action.pixiElem.position.set(this.circle.position.x + 50* Math.cos(i*2*Math.PI/(size + 1)), this.circle.position.y + 50*Math.sin(i*2*Math.PI/(size + 1))) ;
+                    const action: ActionUiElement = new ActionUiElement("Assets/Images/Skills/Bubble.png" , this.currentMonster.skillList[i].name);
+                    action.pixiElem.position.set(this.circle.position.x + 60* Math.cos(i*2*Math.PI/10), this.circle.position.y + 60*Math.sin(i*2*Math.PI/10)) ;
                     this.circle.addChild(action.pixiElem);
                 }
                 this.container.addChild(this.circle);
@@ -45,15 +48,38 @@ class ActionUiElement{
     constructor(picture:string, text:string){
         this.pixiElem = new PIXI.Container();
         this.pic = PIXI.Sprite.from(picture);
-        this.pic.scale.set(0.15, 0.125);
+        this.pic.scale.set(0.4, 0.4);
         this.button = new PIXI.Text(text);
 
         this.button.interactive = true;
         this.button.buttonMode = true;
-        //this.button.anchor.set(0.5,0.5);
         //this.button.on("click", ()=> new AttackAction().execute());
-        //this.button.position.set(this.pic.position.x + 50, this.pic.position.y + 25);
+        this.button.position.set(this.pic.position.x + 25, this.pic.position.y - 5);
         this.pixiElem.addChild(this.pic);
         this.pixiElem.addChild(this.button);
+
+        GameState.emitter.addEventListener(MouseHoverEvent.type, () =>{
+            this.checkMenuHover();
+        });
+    }
+    private checkMenuHover() {
+        /*var pos = GameState.mousePosition.toDisplayCoords();
+        if(pos.x == this.pixiElem.position.x && pos.y == this.pixiElem.position.y){
+            this.onHover();
+        }
+        else{
+            this.onHoverEnd();
+        }*/
+        this.pixiElem.on('mouseover', ()=>{
+            this.onHover();
+        });
+    }
+    private onHover(){
+        this.pixiElem.filters = [(new GlowFilter({ distance: 10, outerStrength: 2, innerStrength: 0, color: 0x99ff99, quality: 0.2 }))];
+        console.log("Mouseover");
+    }
+
+    private onHoverEnd(){
+        this.pixiElem.filters = [];
     }
 }
