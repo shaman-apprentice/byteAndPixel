7a90e157cda645d6a2ab9df5098a9f771fd15d6d
zwarn <michael.budahn@gmail.com>
2020-09-30 17:52:54 +0200

    make GameState instantiable

--- src/GameState.ts
+++ src/GameState.ts
@@ -4,15 +4,57 @@ import { MonsterRemoveEvent } from 'controller/events/MonsterRemoveEvent';
 import { SelectedMonsterChangedEvent } from 'controller/events/SelectedMonsterChangedEvent';
 import { Monster } from 'model/Monster';
 import { getInitialMonsters } from 'controller/monster';
+import * as _ from "lodash";
+import { HashMap } from 'utils/HashMap';
 
 export class GameState {
+  private static _instance : GameState = new GameState(new TileMap(16), getInitialMonsters(), 1, undefined);
   public static emitter = new EventTarget();
 
-  public static map = new TileMap(16);
-  public static monsters = getInitialMonsters();
-  public static turn = 1;
-  private static _selectedMonster = undefined;
+  private _map : TileMap;
+  private _monsters : HashMap<number, Monster>;
+  private _turn: number;
+  private _selectedMonster : Monster;
 
+  constructor (map: TileMap, monsters: HashMap<number, Monster>, turn: number, selectedMonster: Monster) {
+    this._map = map;
+    this._monsters = monsters;
+    this._turn = turn;
+    this._selectedMonster = selectedMonster;
+  }
+  
+  public static set map(map : TileMap) {
+    GameState._instance._map = map;
+  }
+
+  public static get map() {
+    return GameState._instance._map;
+  }
+
+  public static set monsters(monsters: HashMap<number, Monster>) {
+    GameState._instance._monsters = monsters;
+  }
+
+  public static get monsters() {
+    return GameState._instance._monsters;
+  }
+
+  public static set turn(turn: number) {
+    GameState._instance._turn = turn;
+  }
+
+  public static get turn() {
+    return GameState._instance._turn;
+  }
+
+  public static set selectedMonster(monster: Monster) {
+    GameState._instance._selectedMonster = monster;
+    SelectedMonsterChangedEvent.dispatch(monster);
+  }
+  
+  public static get selectedMonster() {
+    return GameState._instance._selectedMonster;
+  }
 
   static addMonster(monster: Monster) {
     GameState.monsters.set(monster.id, monster);
@@ -26,16 +68,5 @@ export class GameState {
       GameState.selectedMonster = GameState.monsters.getValues().find(candiate => candiate.friendly && candiate.id != monster.id)
     }
   }
-
-  static get selectedMonster() {
-    return this._selectedMonster;
-  }
-
-  static set selectedMonster(monster: Monster) {
-    this._selectedMonster = monster;
-    SelectedMonsterChangedEvent.dispatch(monster);
-  }
-
-
 
 }
